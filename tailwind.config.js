01cb8055eb331483a7cd67d068fa056268948d5a
umaygass <browntotoy56@gmail.com>
2025-08-08 13:57:03 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'deped-green': '#228B22',
+          'deped-cream': '#F5F5DC',
+          'deped-brown': '#8B4513',
+          'light-green': '#90EE90',
+          'dark-green': '#006400',
+        },
+      },
+    },
+  }
